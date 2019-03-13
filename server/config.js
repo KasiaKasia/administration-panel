75703ef960584fa5f65edffd1619c44500ecea86
KasiaKasia <bulicka.alexandra@gmail.com>
2019-03-13 19:05:54 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-   'serverport': 4200, // 49156/  4200
+   'serverport': 4200, 
    'tokenexp': 3600,
    'secret': 'mysecretkey',
    'database': 'mongodb://localhost:27017/mean'
